db4d9b8b382a512d5884794d453c767444b3a308
Ben Black <weepingwillowben@gmail.com>
2021-08-01 07:58:52 -0600

    fixed NFSF env loop

--- neural_fictitious_selfplay.py
+++ neural_fictitious_selfplay.py
@@ -6,6 +6,8 @@ import random
 from tianshou.data import Batch
 import reverb
 import multiprocessing
+import importlib
+import numpy as np
 
 def start_server(max_size, agents):
     all_tables = []
@@ -27,6 +29,7 @@ def start_server(max_size, agents):
 
     server = reverb.Server(tables=all_tables, port=8000)
 
+
 def run_server(max_size, agents):
     proc = multiprocessing.Process(target=start_server, args=(max_size, agents))
 
@@ -40,8 +43,9 @@ def add_element(traj_writer, agent, obs, action, reward, done):
           'b': writer.history['b'][:],
       })
 
+
 def make_env(env_name):
-    env = importlib.import_module('pettingzoo.atari.{}'.format(env_name)).env(obs_type='grayscale_image')
+    env = importlib.import_module('pettingzoo.atari.{}'.format(env_name)).parallel_env(obs_type='grayscale_image')
     env = ss.max_observation_v0(env, 2)
     env = ss.frame_skip_v0(env, 4)
     # env = InvertColorAgentIndicator(env) # handled by body
@@ -50,51 +54,82 @@ def make_env(env_name):
     return env
 
 
-class ReservourBuffer:
-    def __init__(self, buffer, size):
-        self.buffer = buffer
-        self.index = 0
-        self.fill_index = 0
-        self.buf_size = size
-
-    def add(self, obs):
-        if self.fill_index < self.buf_size:
-            self.buffer[self.fill_index] = obs
-            self.fill_index += 1
-        else:
-            new_idx = random.randint(0, self.index)
-            if new_idx < self.buf_size:
-                self.buffer[new_idx] = obs
-        self.index += 1
-
-    def sample(self, batch_size):
-        idxs = torch.randint(self.fill_index, size=batch_size)
-        return self.buffer[idxs]
+def make_vec_env(base_env, num_envs, num_cpus):
+    base_env = ss.black_death_v1(base_env)
+    env = ss.pettingzoo_env_to_vec_env_v0(base_env)
+    env = ss.concat_vec_envs_v0(env, 4, num_cpus=2, base_class='stable_baselines3')
+    return env, base_env.possible_agents
 
 
-class QueueBuffer:
-    def __init__(self, buffer, size):
-        self.buffer = buffer
-        self.index = 0
-        self.fill_index = 0
-        self.buf_size = size
+class RandomAgent:
+    def act(self, obs):
+        assert obs.obs.shape[1:] == (1, 84, 84)
 
-    def add(self, obs):
-        if self.fill_index < self.buf_size:
-            self.fill_index += 1
-
-        self.buffer[self.index] = obs
-        self.index += 1
-        if self.index >= self.buf_size:
-            self.index = 0
-
-    def sample(self, batch_size):
-        idxs = torch.randint(self.fill_index, size=batch_size)
-        return self.buffer[idxs]
+        return torch.randint(high=18, size=(obs.obs.shape[0],))
 
 
-#TODO: implement anticipory dynamics
+def apply_all_in_batch(batch, op):
+    batch_dict = {}
+    for name, tensor in batch.__dict__.items():
+        batch_dict[name] = op(tensor)
+    return Batch(batch_dict)
 
+
+class AgentDistributor:
+    '''
+    Maps multi-agent flattened vector env layout onto multiple agents.
+    '''
+    def __init__(self, agents):
+        self.agents = agents
+        self.num_agents = len(self.agents)
+
+    def act(self, all_observations):
+        reshaped_obs = apply_all_in_batch(all_observations, lambda obs: obs.view((obs.shape[0]//self.num_agents,self.num_agents,)+obs.shape[1:]))
+        agent_first_obs = apply_all_in_batch(reshaped_obs, lambda obs: torch.swapaxes(obs,0,1))
+        agent_actions = []
+        for i, agent in enumerate(self.agents):
+            actions = agent.act(agent_first_obs[i])
+            agent_actions.append(actions)
+        vec_env_actions = torch.stack(agent_actions,axis=1)
+        assert vec_env_actions.shape[:2] == reshaped_obs.obs.shape[:2]
+        vec_env_actions = vec_env_actions.view((all_observations.obs.shape[0],)+vec_env_actions.shape[2:])
+        return vec_env_actions
+
+
+def obs_are_zeros(batched_obs):
+    '''
+    check if observations are zeros (mean black death, means they should not be learned)
+    '''
+    return torch.all(torch.eq(batched_obs.view(batched_obs.shape[0], -1), 0), dim=1).type(torch.uint8)
+
+
+def make_obs_batch_from_env(observations, rewards, dones, device):
+    torch_obs = torch.tensor(observations, device=device)
+    torch_rews = torch.tensor(rewards, device=device, dtype=torch.float32)
+    torch_dones = torch.tensor(dones, device=device, dtype=torch.uint8)
+    torch_mask = 1 - (torch_dones | obs_are_zeros(torch_obs))
+    return Batch({
+        "obs": torch_obs,
+        "rewards": torch_rews,
+        "dones": torch_dones,
+        "mask": torch_mask,
+    })
+
+def env_loop(env_name, num_steps, num_envs, device="cpu"):
+    base_env = make_env(env_name)
+    v_env, agent_names = make_vec_env(base_env, num_envs, num_cpus=4)
+    num_agents = len(agent_names)
+    agents = AgentDistributor([RandomAgent() for i in range(num_agents)])
+
+    observations = v_env.reset()
+    rewards = np.zeros(len(observations))
+    dones = np.zeros(len(observations))
+    for i in range(num_steps):
+        batched_data = make_obs_batch_from_env(observations, rewards, dones, device)
+        actions = agents.act(batched_data)
+        observations, rewards, dones, infos = v_env.step(actions)
+
+env_loop("space_invaders_v1",1000,4)
 
 def rl_model(frames=4, hidden=512, atoms=51, sigma=0.5):
     return nn.Sequential(
