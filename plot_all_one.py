ca37281de3451cd1a529768de565d9c867f2e4f7
Ben Black <weepingwillowben@gmail.com>
2021-09-11 14:01:29 -0600

    added ability to plot multiple algorithms in one csv file

--- plot_all_one.py
+++ plot_all_one.py
@@ -11,6 +11,12 @@ import json
 
 from all_envs import all_environments
 
+def get_algo_name(experiment_name):
+    if 'shared_ppo' in experiment_name:
+        return 'shared_ppo'
+    else:
+        return "shared_rainbow"
+
 def get_env_name(experiment_name):
     for env_name in all_environments:
         if env_name in experiment_name:
@@ -76,9 +82,10 @@ def get_exp_label(exp):
 
 def main():
 
-    assert len(sys.argv) == 2, "must supply name of csv file as argument"
+    assert len(sys.argv) == 3, "must supply name of csv file and vs_random as arguments"
 
     csv_name = sys.argv[1]
+    vs_random = bool(sys.argv[2])
 
 
     matplotlib.use("pgf")
@@ -95,6 +102,8 @@ def main():
     plt.figure(figsize=(2.65*3*1.0, 1.5*7*1.0))
 
     csv_data = pandas.read_csv(csv_name)
+    # print(csv_data['vs_random'])
+    csv_data = csv_data[csv_data['vs_random'] == vs_random]
     csv_data['no_seed_experiment'] = [s.rsplit('_', 1)[0] for s in csv_data['experiment']]
     print(len(csv_data['no_seed_experiment']))
     print(len(set(csv_data['no_seed_experiment'])))
@@ -114,11 +123,11 @@ def main():
         'agent3_rew': ['mean', 'min', 'max'],
         'agent4_rew': ['mean', 'min', 'max'],
     })
-    print(accumed)
+    # print(accumed)
     accumed.reset_index(inplace=True)
-    print(len(csv_data['agent1_rew']))
-    print(len(accumed['agent1_rew']['mean']))
-    return
+    # print(len(csv_data['agent1_rew']))
+    # print(len(accumed['agent1_rew']['mean']))
+    # return
     # print(accumed['agent1_rew']['mean'])
     # print(accumed)
     # return
@@ -126,29 +135,41 @@ def main():
     random_data = json.load(open("plot_data/rand_rewards.json"))
     csv_data = accumed#[(accumed['agent'] == "first_0")]
     #print(data)
-    all_envs = sorted(set(csv_data['no_seed_experiment']))
+    all_envs = list(sorted(set(csv_data['no_seed_experiment'])))
 
     all_env_map = {get_env_name(env): env for env in all_envs}
+    all_algo_env_map = {(get_env_name(env),get_algo_name(env)): env for env in all_envs}
+    all_algo_names = {get_algo_name(env) for env in all_envs}
+    print(all_algo_env_map)
     all_envs = sorted(all_env_map.keys(), key=str.lower)
-
+    print(all_algo_names)
+    algo_lines = {}
+    color_map = {
+        'shared_ppo': "blue",
+        'shared_rainbow': "orange",
+    }
     plot_ind = 1
     for env in all_envs:
-        df = csv_data[(csv_data['no_seed_experiment'] == all_env_map[env])]
         plt.subplot(8,3,plot_ind)
-        ax = plt.gca()
-        rand_reward = random_data[env]['mean_rewards']['first']
+        # ax = plt.gca()
         #df = pd.read_csv(os.path.join(data_path, env+'.csv'))
         # data = df.to_numpy()
         #filtered = scipy.signal.savgol_filter(data[:, 1], int(len(data[:, 1])/110)+2, 5)
-        x_axis = df['checkpoint'].to_numpy()
-        mean_val = df['agent1_rew']['mean'].to_numpy()
-        max_val = df['agent1_rew']['max'].to_numpy()
-        min_val = df['agent1_rew']['min'].to_numpy()
-        line, = plt.plot(x_axis, mean_val, label=env, linewidth=0.6, color='#0530ad', linestyle='-')
-        # print(mean_val, max_val, min_val)
-        ax.fill_between(x_axis, min_val, max_val, alpha=0.3, facecolor='blue')
+        for algo in all_algo_names:
+            df = csv_data[(csv_data['no_seed_experiment'] == all_algo_env_map[(env,algo)])]
+            x_axis = df['checkpoint'].to_numpy()
+            # print('\n'.join(str(v1) + v2 for v1, v2 in zip(df['checkpoint'],df['no_seed_experiment'])))
+            mean_val = df['agent1_rew']['mean'].to_numpy()
+            max_val = df['agent1_rew']['max'].to_numpy()
+            min_val = df['agent1_rew']['min'].to_numpy()
+            line, = plt.plot(x_axis, mean_val, label=env, linewidth=0.6, color=color_map[algo], linestyle='-')
+            algo_lines[algo] = line
+            # print(mean_val, max_val, min_val)
+            plt.fill_between(x_axis, min_val, max_val, alpha=0.3, facecolor=color_map[algo])
         # plt.set_ylabel('between y1 and 0')
+        rand_reward = random_data[env]['mean_rewards']['first']
         rand_line, = plt.plot(x_axis,rand_reward*np.ones_like(x_axis), label=env, linewidth=0.6, color='#A0522D', linestyle='-')
+
         plt.xlabel('Steps', labelpad=1)
         plt.ylabel('Average Total Reward', labelpad=1)
         plt.title(get_exp_label(env))
@@ -161,8 +182,13 @@ def main():
         plt.margins(x=0)
         plot_ind += 1
 
-
-    plt.figlegend([line,rand_line],['Trained Agent vs Random Agent', "Random Agent vs Random Agent"], fontsize='x-large', loc='lower center', ncol=1, labelspacing=.2, columnspacing=.25, borderpad=.25, bbox_to_anchor=(0.68,0.06))
+    name_map = {
+        'shared_ppo': "PPO Agent vs Random Agent",
+        'shared_rainbow': "Rainbow Agent vs Random Agent",
+    }
+    name_sublist = [name_map[algo] for algo in all_algo_names]
+    lint_sublist = [algo_lines[algo] for algo in all_algo_names]
+    plt.figlegend(lint_sublist+[rand_line],name_sublist + [ "Random Agent vs Random Agent"], fontsize='x-large', loc='lower center', ncol=1, labelspacing=.2, columnspacing=.25, borderpad=.25, bbox_to_anchor=(0.68,0.06))
     plt.savefig(f"{csv_name}.pgf", bbox_inches = 'tight',pad_inches = .025)
     plt.savefig(f"{csv_name}.png", bbox_inches = 'tight',pad_inches = .025, dpi=600)
 
